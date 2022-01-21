08cb7fe5d8265521b84633bef12aeee7acb944d9
nastassia <anastasiyamihalenko@gmail.com>
2022-01-21 23:42:11 +0300

    added styles for footer, fix style for nav

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import classes from './Footer.module.css'
 import {Project} from "../projects/project/Project";
+import linkedin from '../assets/Skills/linkedin.png'
+import github from '../assets/Skills/github.png'
+import skype from '../assets/Skills/skype.png'
 
 
 export const Footer = () => {
     return (
         <footer className={classes.footerBlock}>
             <div className={classes.containerFooter}>
-                <h2 className={classes.titleFooter}>Links</h2>
+                {/*<h2 className={classes.titleFooter}>Links</h2>*/}
                 <div className={classes.linksWrapper}>
-                    <p><a href={`https://github.com/NastassiaMikhalenka`}><img
-                        src={`https://www.veryicon.com/download/png/application/outline-1/github-58?s=256`} width="50"
-                        height="50" alt="Пример"/></a></p>
-                    <p><a href={`https://www.linkedin.com/in/nastassia-mikhalenka-790062212/`}><img
-                        src={`https://www.veryicon.com/download/png/application/outline-1/github-58?s=256`} width="50"
-                        height="50" alt="Пример"/></a></p>
-                    <p><a href={`mailto:anastasiyamihalenko@gmail.com`}><img
-                        src={`https://www.veryicon.com/download/png/application/outline-1/github-58?s=256`} width="50"
-                        height="50" alt="Пример"/></a></p>
+                    <p><a href="https://www.linkedin.com/in/nast-mikh/">
+                        <img src={linkedin} alt="linkedin"/></a>
+                    </p>
+                    <p><a href="https://github.com/NastassiaMikhalenka">
+                        <img src={github} alt="linkedin"/></a>
+                    </p>
+                    <p><a href="https://join.skype.com/invite/CChGvYPRcrjg">
+                        <img src={skype} alt="linkedin"/></a>
+                    </p>
                 </div>
             </div>
         </footer>
