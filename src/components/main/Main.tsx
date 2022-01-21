b83a60023758deef086a1e3d08e8c4cc2b080ff7
nastassia <anastasiyamihalenko@gmail.com>
2022-01-21 21:00:47 +0300

    added styles for mainContainer

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -6,11 +6,21 @@ export const Main = () => {
         <main className={classes.mainBlock}>
             <div className={classes.containerMain}>
                 <div className={classes.greetings}>
-                    <span>Hello,</span>
-                    <h1>Nastya</h1>
-                    <p>a freelance FE developer</p>
+                    <div className={classes.greetings_ContainerName}>
+                        <span className={classes.greetings_Name}>Nastassia</span>
+                    </div>
+                    <h2 className={classes.greetings_Vac}>Front end developer</h2>
+                    <p className={classes.greetings_Text}>Hi, I’m Nastassia. I'm a purposeful and highly motivated person.
+                        If you are looking for FE developer to build your brands and grow your business Let’s shake hands with me.</p>
+                    <div className={classes.greetings_ContainerButtons}>
+                        <button className={classes.greetings_ButtonHireMe}>Hire me</button>
+                        <button className={classes.greetings_ButtonReadMore}>Read more</button>
+                    </div>
+                    <hr/>
+                    <p className={classes.greetings_TextBottom}>I am currently open for full-time work.</p>
                 </div>
-                <div className={classes.photoMain}></div>
+                <div className={classes.photoMain}>
+                </div>
             </div>
         </main>
     )
