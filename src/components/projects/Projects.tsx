79ca857fb4a4b402c34decac08c5f4acd02a1b3c
nastassia <anastasiyamihalenko@gmail.com>
2022-01-23 23:47:59 +0300

    added carousel

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -4,22 +4,31 @@ import classes from './Projects.module.css';
 import {Project} from "./project/Project";
 
 export const Projects = () => {
+    const project1 = [
+        {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear2.jpeg`,},
+        {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear1.jpeg`},
+        {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear3.jpeg`,},
+    ];
+    const project2 = [
+        {image: `https://img5.goodfon.ru/wallpaper/big/4/79/ozero-zakat-tuman-zima.jpg`,},
+        {image: `https://img5.goodfon.ru/wallpaper/big/4/bf/andre-furtado-canada-city-street-snowfall-lights-buildings-p.jpg`},
+        {image: `https://u01.appmifile.com/images/2017/04/15/db874b01-7402-4899-afba-e7a29c8b6c0e.jpg`,},
+    ];
+
     return (
         <section className={classes.projectsBlock}>
             <div className={classes.containerProjects}>
                 <h2 className={classes.titleProjects}>Projects</h2>
                 <div className={classes.projects}>
-                    <Project title={"HTML"}
-                           description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
+                    <Project
+                        images={project1}
+                        title={"HTML"}
+                        description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
                     />
-                    <Project title={"HTML"}
-                             description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
-                    />
-                    <Project title={"HTML"}
-                             description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
-                    />
-                    <Project title={"HTML"}
-                             description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
+                    <Project
+                        images={project2}
+                        title={"HTML"}
+                        description={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
                     />
                 </div>
             </div>
--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './Project.module.css'
-import github from './../../assets/Skills/github.png'
 import arrow_right from './../../assets/Skills/arrow-right.png'
 
 
 type PropsType = {
     title?: string
     description: string
+    images: any
 }
 
 export const Project = (props: PropsType) => {
+    const [imageData] = useState(props.images);
+    const [current, setCurrent] = useState(0);
+    const [quote, getQuote] = useState(imageData[current])
+
+    useEffect(
+        () => getQuote(imageData[current]),
+        [current, quote]
+    )
+
+    const nextQuote = () => {
+        if (current === imageData.length - 1) {
+            setCurrent(0)
+        } else {
+            setCurrent(current + 1)
+        }
+    }
+
+    const prevQuote = () => {
+        if (current === 0) {
+            setCurrent(imageData.length - 1)
+        } else {
+            setCurrent(current - 1)
+        }
+    }
+
     return (
-        <div className={classes.project}>
-            <div className={classes.iconProject}>
-                {/*<img src={`https://st3.depositphotos.com/1010735/31707/v/600/depositphotos_317075756-stock-illustration-realistic-black-laptop-mockup-with.jpg`} alt={"#"} width="200"/>*/}
-                {/*<a href={"https://www.google.by/"} target="_blank"><img src={github}/></a>*/}
-                <img src={"https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear2.jpeg"} alt={"#"}/>
-            </div>
-            <h2>{props.title}</h2>
-            <div className={classes.description}>
-            <p>{props.description}</p>
-                 <img src={arrow_right} alt={"2"}/>
+        <div>
+            <div className={classes.project}>
+                <div className={classes.iconProject}>
+                    <img src={quote.image} alt={"#"}/>
+                    <a onClick={prevQuote} className={classes.prev} id="prev">&#10094;</a>
+                    <a onClick={nextQuote} className={classes.next} id="next">&#10095;</a>
                 </div>
+                <h2>{props.title}</h2>
+                <div className={classes.description}>
+                    <p>{props.description}</p>
+                    <img src={arrow_right} alt={"2"}/>
+                </div>
+            </div>
         </div>
     )
 }
