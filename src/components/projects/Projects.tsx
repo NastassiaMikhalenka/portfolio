d0fa52ad102488313518b5ab7eaad1a1fbaeadfa
nastassia <anastasiyamihalenko@gmail.com>
2022-03-24 01:39:04 +0300

    added projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,63 +2,16 @@ import React from 'react';
 // @ts-ignore
 import classes from './Projects.module.css';
 import {Project} from "./project/Project";
-import bite1 from '../assets/ProjectsImg/Bite/bite1.jpeg'
-import bite2 from '../assets/ProjectsImg/Bite/bite2.jpeg'
-import bite3 from '../assets/ProjectsImg/Bite/bite3.jpeg'
-import bite4 from '../assets/ProjectsImg/Bite/bite4.jpeg'
-import Trevland1 from '../assets/ProjectsImg/Trevland/Trevland1.jpeg'
-import Trevland2 from '../assets/ProjectsImg/Trevland/Trevland2.jpeg'
-import Trevland3 from '../assets/ProjectsImg/Trevland/Trevland3.jpeg'
+import {project1, project2, project3, project4, project5} from "./ProjectsDescription";
 
 export const Projects = () => {
-    const project1 = {
-        id: 1,
-        title: 'The card game memory',
-        description: 'It is a long established fact that a reader will be',
-        image: [
-            {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear2.jpeg`,},
-            {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear1.jpeg`},
-            {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear3.jpeg`,},
-        ],
-        link: 'https://nastassiamikhalenka.github.io/new-year-game/',
-        linkGitHub: 'https://nastassiamikhalenka.github.io/traveland/',
-    }
-    const project2 = {
-        id: 1,
-        title: 'Landing Page Trevland',
-        description: 'This landing page for travel agency. There are also a few areas with JS. This landing page for travel agency. There are also a few areas with JS.',
-        image: [
-            {image: Trevland1,},
-            {image: Trevland2,},
-            {image: Trevland3,},
-        ],
-        link: 'https://nastassiamikhalenka.github.io/traveland/',
-        linkGitHub: 'https://nastassiamikhalenka.github.io/traveland/',
-
-    }
-    const project3 = {
-        id: 1,
-        title: 'E-commerce Shop',
-        description: 'Created shopping store by React & Redux Created shopping store by React & Redux.',
-        image: [
-            {image: bite3,},
-            {image: bite4,},
-            {image: bite2,},
-            {image: bite1,},
-        ],
-        link: 'https://NastassiaMikhalenka.github.io/bite',
-        linkGitHub: 'https://nastassiamikhalenka.github.io/traveland/',
-    }
-
     return (
         <section className={classes.projectsBlock} id={'Projects'}>
             <div className={classes.containerProjects}>
                 <h2 className={classes.titleProjects}>Projects</h2>
                 <div className={classes.projects}>
-                    <Project project={project2}/>
-                    <Project project={project2}/>
-                    <Project project={project2}/>
-                    <Project project={project3}/>
+                    <Project project={project4}/>
+                    <Project project={project5}/>
                     <Project project={project1}/>
                     <Project project={project2}/>
                     <Project project={project3}/>
