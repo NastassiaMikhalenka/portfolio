71a15c4e336d1be0813fa0a4bac543622aeb81aa
nastassia <anastasiyamihalenko@gmail.com>
2022-03-24 01:39:17 +0300

    added files for projects

---
+++ src/components/projects/ProjectsDescription.ts
@@ -0,0 +1,86 @@
+import bite1 from '../assets/ProjectsImg/Bite/bite1.jpeg'
+import bite2 from '../assets/ProjectsImg/Bite/bite2.jpeg'
+import bite3 from '../assets/ProjectsImg/Bite/bite3.jpeg'
+import bite4 from '../assets/ProjectsImg/Bite/bite4.jpeg'
+import Trevland1 from '../assets/ProjectsImg/Trevland/Trevland1.jpeg'
+import Trevland2 from '../assets/ProjectsImg/Trevland/Trevland2.jpeg'
+import Trevland3 from '../assets/ProjectsImg/Trevland/Trevland3.jpeg'
+import QuizCards1 from '../assets/ProjectsImg/QuizCards/QuizCards1.jpeg'
+import QuizCards2 from '../assets/ProjectsImg/QuizCards/QuizCards2.png'
+import QuizCards3 from '../assets/ProjectsImg/QuizCards/QuizCards3.png'
+import QuizCards4 from '../assets/ProjectsImg/QuizCards/QuizCards4.png'
+import QuizCards5 from '../assets/ProjectsImg/QuizCards/QuizCards5.png'
+import QuizCards6 from '../assets/ProjectsImg/QuizCards/QuizCards6.png'
+import AviaSales1 from '../assets/ProjectsImg/Aviasales/Aviasales1.png'
+import AviaSales2 from '../assets/ProjectsImg/Aviasales/Aviasales2.png'
+import AviaSales3 from '../assets/ProjectsImg/Aviasales/Aviasales3.png'
+
+
+
+export const project1 = {
+    id: 1,
+    title: 'The card game memory',
+    description: 'It is a long established fact that a reader will be',
+    image: [
+        {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear2.jpeg`,},
+        {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear1.jpeg`},
+        {image: `https://github.com/NastassiaMikhalenka/new-year-game/raw/master/src/assets/NewYear3.jpeg`,},
+    ],
+    link: 'https://nastassiamikhalenka.github.io/new-year-game/',
+    linkGitHub: 'https://github.com/NastassiaMikhalenka/new-year-game',
+}
+export const project2 = {
+    id: 1,
+    title: 'Landing Page Trevland',
+    description: 'This landing page for travel agency. There are also a few areas with JS. This landing page for travel agency. There are also a few areas with JS.',
+    image: [
+        {image: Trevland1,},
+        {image: Trevland2,},
+        {image: Trevland3,},
+    ],
+    link: 'https://nastassiamikhalenka.github.io/traveland/',
+    linkGitHub: 'https://nastassiamikhalenka.github.io/traveland/',
+}
+
+export const project3 = {
+    id: 1,
+    title: 'E-commerce Shop',
+    description: 'Created shopping store by React & Redux Created shopping store by React & Redux.',
+    image: [
+        {image: bite3,},
+        {image: bite4,},
+        {image: bite2,},
+        {image: bite1,},
+    ],
+    link: 'https://NastassiaMikhalenka.github.io/bite',
+    linkGitHub: 'https://github.com/NastassiaMikhalenka/bite',
+}
+
+export const project4 = {
+    id: 1,
+    title: 'Quiz Cards',
+    description: 'Teamwork. Created app by React & Redux',
+    image: [
+        {image: QuizCards1,},
+        {image: QuizCards2,},
+        {image: QuizCards3,},
+        {image: QuizCards4,},
+        {image: QuizCards5,},
+        {image: QuizCards6,},
+    ],
+    link: 'https://nastassiamikhalenka.github.io/projectcards/',
+    linkGitHub: 'https://github.com/NastassiaMikhalenka/projectcards',
+}
+
+export const project5 = {
+    id: 1,
+    title: 'AviaSales',
+    description: 'Created app by React & Redux',
+    image: [
+        {image: AviaSales1,},
+        {image: AviaSales2,},
+        {image: AviaSales3,},
+    ],
+    link: 'https://nastassiamikhalenka.github.io/aviasales/',
+    linkGitHub: 'https://github.com/NastassiaMikhalenka/aviasales',
+}
